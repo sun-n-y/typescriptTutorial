d1286a7a8e57f399ce41ed93be50f739666868dc
sunny patel <sunnykpatel.skp@gmail.com>
2024-05-20 14:24:16 -0400

    add type anotations

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,1 +1,58 @@
-console.log('hi');
+// // type annotations
+
+// let name: string = 'sunny';
+// name = name.toLocaleUpperCase();
+// console.log(name);
+
+// let cost: number = 12;
+// cost++;
+// console.log(cost);
+
+// let isHome: boolean = false;
+// isHome = false;
+// console.log(isHome);
+
+// console.log(name);
+
+// //union type
+
+// let tax: number | string = 10;
+
+// tax = 100;
+// tax = 'fs';
+
+// let requestStatus: 'pending' | 'success' | 'error' = 'pending';
+
+// requestStatus = 'pending';
+
+// //type any
+
+// let notSure: any = 4;
+// notSure = 'sdf';
+// notSure = 3;
+// notSure = false;
+
+// //example
+
+// const books = ['hello', 'hi', 'hole'];
+
+// let foundBook: string | undefined;
+
+// for (let book of books) {
+//   if (book === 'hi') {
+//     foundBook = book;
+//     break;
+//   }
+// }
+
+// foundBook = foundBook?.toUpperCase();
+// console.log(foundBook);
+
+let orderStatus: 'processing' | 'shipped' | 'delivered' = 'delivered';
+
+orderStatus = 'shipped';
+orderStatus = 'delivered';
+
+let discount: number | string = 20;
+
+discount = '20%';
