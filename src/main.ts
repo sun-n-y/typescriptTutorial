93c268f42969ba3cd1cd44463da20f44b4df353e
sunny patel <sunnykpatel.skp@gmail.com>
2024-05-20 11:29:35 -0400

    add checkpoint

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import typescriptLogo from './typescript.svg';
 import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.ts';
 import './tutorial.ts';
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="/tasks" target="_blank">
