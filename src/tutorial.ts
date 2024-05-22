2f4fcc4f33360652b10bf8f447e4307129777a90
sunny patel <sunnykpatel.skp@gmail.com>
2024-05-22 12:20:28 -0400

    add checkpoint

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,47 +1,135 @@
-interface Book {
-  readonly isbn: number;
-  title: string;
-  author: string;
-  genre?: string;
-  printAuthor(): void;
-  printTitle(message: string): string;
+// interface Book {
+//   readonly isbn: number;
+//   title: string;
+//   author: string;
+//   genre?: string;
+//   printAuthor(): void;
+//   printTitle(message: string): string;
+// }
+
+// const deepWork: Book = {
+//   isbn: 123,
+//   title: 'deep work',
+//   author: 'cal newport',
+//   genre: 'self-help',
+//   printAuthor() {
+//     console.log(this.author);
+//   },
+//   printTitle(message) {
+//     return `${this.title} ${message}`;
+//   },
+// };
+
+// deepWork.printAuthor();
+// const result = deepWork.printTitle('hello');
+// console.log(result);
+
+// interface Computer {
+//   readonly id: number;
+//   brand: string;
+//   ram: number;
+//   storage?: number;
+//   upgradeRam(number: number): number;
+// }
+
+// const object1: Computer = {
+//   id: 1,
+//   brand: 'apple',
+//   ram: 16,
+//   upgradeRam(number) {
+//     this.ram += number;
+//     return this.ram;
+//   },
+// };
+
+// console.log(object1);
+// console.log(object1.upgradeRam(16));
+// console.log(object1);
+
+// //interface merging and extend
+
+// interface Person {
+//   name: string;
+//   getDetails(): string;
+// }
+
+// interface DogOwner {
+//   dogName: string;
+//   getDogDetails(): string;
+// }
+
+// interface Person {
+//   age: number;
+// }
+
+// const person: Person = {
+//   name: 'john',
+//   age: 3,
+//   getDetails() {
+//     return `Name: ${this.name} ${this.age}`;
+//   },
+// };
+
+// interface Employee extends Person {
+//   employeeId: number;
+// }
+
+// const employee: Employee = {
+//   name: 'tim',
+//   age: 90,
+//   employeeId: 123,
+//   getDetails() {
+//     return `${this.name} ${this.age} ${this.employeeId}`;
+//   },
+// };
+
+// interface Manager extends Person, DogOwner {
+//   managePeople(): void;
+// }
+
+// const manager: Manager = {
+//   name: 'jim',
+//   age: 35,
+//   dogName: 'rex',
+
+//   getDetails() {
+//     return `${this.name} ${this.age}`;
+//   },
+
+//   getDogDetails() {
+//     return `${this.dogName}`;
+//   },
+//   managePeople() {
+//     console.log(`managing people`);
+//   },
+// };
+
+// console.log(manager);
+
+interface Person {
+  name: string;
 }
 
-const deepWork: Book = {
-  isbn: 123,
-  title: 'deep work',
-  author: 'cal newport',
-  genre: 'self-help',
-  printAuthor() {
-    console.log(this.author);
-  },
-  printTitle(message) {
-    return `${this.title} ${message}`;
-  },
-};
-
-deepWork.printAuthor();
-const result = deepWork.printTitle('hello');
-console.log(result);
-
-interface Computer {
-  readonly id: number;
-  brand: string;
-  ram: number;
-  storage?: number;
-  upgradeRam(number: number): number;
+interface DogOwner extends Person {
+  dogName: string;
 }
 
-const object1: Computer = {
-  id: 1,
-  brand: 'apple',
-  ram: 16,
-  upgradeRam(number) {
-    this.ram += number;
-    return this.ram;
-  },
-};
+interface Manager extends Person {
+  managePeople(): void;
+  delegateTasks(): void;
+}
 
-console.log(object1);
-console.log(object1.upgradeRam(16));
-console.log(object1);
+function getEmployee(): Person | Manager | DogOwner {
+  const randomNumber = Math.random();
+  if (randomNumber < 0.33) {
+    return { name: 'polo' };
+  } else if (randomNumber < 0.66) {
+    return { name: 'tim', dogName: 'rex' };
+  } else {
+    return { name: 'boss', managePeople() {}, delegateTasks() {} };
+  }
+}
+
+const employee: Person | DogOwner | Manager = getEmployee();
+
+console.log(employee);
